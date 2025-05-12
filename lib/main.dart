f5473292a50b2f05a68b79166d37d4a001f574a3
Romain Liénard <rhum1@MacBook-Air-de-Romain.local>
2025-05-12 14:37:46 +0200

    fix: login page not displayed

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,19 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:my_first_project/screens/search_screen.dart';
 import 'package:provider/provider.dart';
+import 'auth_page.dart';
+import 'firebase_options.dart';
 import 'providers/favorites_provider.dart';
 import 'screens/home_screen.dart';
 import 'screens/favorites_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   final favProvider = FavoritesProvider();
   await favProvider.loadFromStorage();
 
@@ -18,19 +25,55 @@ void main() async {
   );
 }
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
-  State<MyApp> createState() => _MyAppState();
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Music',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(primarySwatch: Colors.green),
+      home: const AuthWrapper(),
+    );
+  }
 }
 
-class _MyAppState extends State<MyApp> {
+class AuthWrapper extends StatelessWidget {
+  const AuthWrapper({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<User?>(
+      stream: FirebaseAuth.instance.authStateChanges(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Scaffold(
+            body: Center(child: CircularProgressIndicator()),
+          );
+        }
+        if (snapshot.hasData) {
+          return const MainScreen();
+        }
+        return const AuthPage();
+      },
+    );
+  }
+}
+
+class MainScreen extends StatefulWidget {
+  const MainScreen({super.key});
+
+  @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
   int _selectedIndex = 0;
   final List<Widget> _screens = [
-    const HomeScreen(),
-    const FavoritesScreen(),
-    const SearchScreen(),
+    HomeScreen(),
+    FavoritesScreen(),
+    SearchScreen(),
   ];
 
   void _onTap(int index) => setState(() => _selectedIndex = index);
